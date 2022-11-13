088a316e5afe310acf22bc4654bfa2e28f3657cf
Brayan Ramirez <global-sourcecode4546@gmail.com>
2022-11-13 15:21:38 -0500

    commit inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+import  express  from "express";
+import {dirname, join} from "path"
+import {fileURLToPath} from "url";
+
+const app=express();
+// se saca el directorio de nuestro proyecto para utilizarlo
+const __dirname= dirname(fileURLToPath(import.meta.url));
+// se establece el motor de plantillas 
+app.set("view engine",'ejs');
+// se inicializa el servidor
+app.listen(3500);
+
+//  se establece las carpeta que tendra los archivos estaticos
+app.use(express.static(join(__dirname,"public")));
+
+
+app.get ("/",(req,res)=>res.render("index"));
+
+console.log(__dirname);
+//  console.log("hola mundo")
