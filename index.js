4740dd69c927e8848d2789956c45ad51416708bb
Brayan Ramirez <global-sourcecode4546@gmail.com>
2022-11-19 18:47:02 -0500

    se agregan las rutas

--- index.js
+++ index.js
@@ -2,19 +2,20 @@
 import  express  from "express";
 import {dirname, join} from "path"
 import {fileURLToPath} from "url";
+import indexRoutes from "./routes/index.js";
+import backendRoutes from "./routes/backend/registro.js";
 
 const app=express();
 // se saca el directorio de nuestro proyecto para utilizarlo
-const __dirname= dirname(fileURLToPath(import.meta.url));
+const __DIR__=dirname(fileURLToPath(import.meta.url));
 // se establece el motor de plantillas 
 app.set("view engine",'ejs');
 // se inicializa el servidor
 app.listen(3500);
 
-//  se establece las carpeta que tendra los archivos estaticos
-app.use(express.static(join(__dirname,"public")));
+// se establece las carpeta que tendra los archivos estaticos
+app.use(express.static(join(__DIR__,"public")));
+// ruta de formulario osea la index
 
-
-app.get ("/",(req,res)=>res.render("index"));
-console.log(__dirname); 
-console.log(__dirname); 
+app.use(indexRoutes);
+app.use(backendRoutes);
